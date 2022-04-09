a7a85ba7fc7c4388c257c0f1850b712e40333bb0
weiern1997 <weiern1997@gmail.com>
2022-04-09 22:30:33 +0800

    Add gitignore

--- build/ee4308_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pgokul/EE4308_project2/src/ee4308_bringup
-# Build directory: /home/pgokul/EE4308_project2/build/ee4308_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ee4308_hector/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "message_runtime;geometry_msgs;std_srvs;hector_uav_msgs;message_generation;nav_msgs;roscpp;rospy;sensor_msgs;std_msgs;tf2".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "ee4308_hector"
-PROJECT_SPACE_DIR = "/home/pgokul/EE4308_project2/devel"
-PROJECT_VERSION = "0.0.0"
